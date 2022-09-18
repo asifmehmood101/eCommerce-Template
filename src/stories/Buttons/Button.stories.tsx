8bf1709c0a458b8e68185838bb0618ec23692332
asifmehmood101 <amehmood999@gmail.com>
2022-09-18 13:18:48 +0500

    feat: button

--- src/stories/Buttons/Button.stories.tsx
+++ src/stories/Buttons/Button.stories.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Buttons } from "./Buttons";
+import { Button } from "./Buttons";
 
 export default {
     title: "UserAction/Buttons",
-    component: Buttons,
-} as ComponentMeta<typeof Buttons>;
+    component: Button,
+} as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Buttons> = (args) => <Buttons {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 
--- src/stories/Buttons/Buttons.tsx
+++ src/stories/Buttons/Buttons.tsx
@@ -1,42 +1,44 @@
 import React from "react";
-import { Button, ButtonProps } from "@mui/material";
+import { Button as MuiButton, ButtonProps } from "@mui/material";
 import { styled } from "@mui/system";
 
-interface ButtonPropsType {
-    variant: string;
-    size: string;
-    ButtonDisabled?: boolean;
-    eventHandler?: () => void;
-    href?: string;
-    color?: string;
+// types intersection
+type FullButtonType = ButtonProps & {
     label: string;
-}
+};
 
 // styles
-
-const ButtonStyle = styled(Button)(({ theme, color, variant }) => ({
-    // border: "2px solid #46760A",
+const StyledButton = styled(MuiButton)(({ color, variant }) => ({
     borderRadius: "12px",
     fontFamily: "Poppins",
-    fontSize: "0.9rem",
+    fontSize: "0.8rem",
     fontWeight: 700,
     lineHeight: "23px",
-    backgroundColor:
-        color === "secondary"
-            ? variant === "contained"
-                ? theme.palette.secondary.main
-                : "transparent"
-            : variant === "contained"
-            ? theme.palette.primary.main
-            : "transparent",
     color: variant === "contained" ? (color === "secondary" ? "#151515" : "#fff") : "#151515",
     boxShadow: "none",
+    "&:hover": {
+        boxShadow: "none",
+    },
+
+    "&:focus": {
+        boxShadow: "none",
+    },
 }));
 
-export function Buttons({ variant, ButtonDisabled, eventHandler, size, href, color, label, type }: ButtonPropsType & ButtonProps) {
+export function Button({ variant, size, href, color, label, type, disabled, startIcon, endIcon, onClick }: FullButtonType) {
     return (
-        <ButtonStyle variant={variant} disabled={ButtonDisabled} onClick={eventHandler} size={size} href={href} color={color} type={type}>
+        <StyledButton
+            variant={variant}
+            disabled={disabled}
+            size={size}
+            href={href}
+            color={color}
+            type={type}
+            startIcon={startIcon}
+            endIcon={endIcon}
+            onClick={onClick}
+        >
             {label}
-        </ButtonStyle>
+        </StyledButton>
     );
 }
